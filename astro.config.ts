93504edf08e2d8fb0649245e1f7e0d52446be21d
vashmeen <vashmeen@eva.mozmail.com>
2023-11-06 10:21:15 -0800

    feat: add markdown compiler

--- astro.config.ts
+++ astro.config.ts
@@ -2,22 +2,26 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
 import addClasses from './addClasses';
+import mdx from "@astrojs/mdx";
+import markdownIntegration from '@astropub/md'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hamidd.dev',
-  integrations: [sitemap(), UnoCSS()],
-  markdown: {
-    rehypePlugins: [
-      [
-        addClasses,
-        {
-          h1: 'H1',
-          h2: 'H2',
-          h3: 'H3',
-          h4: 'H4',
-          code: 'bg-gray2 px-2 py-1 rd-md c-gray11',
-        },
-      ],
+  integrations: [
+    mdx(),
+    sitemap(),
+    UnoCSS(),
+    markdownIntegration(),
+
     ],
-  },
+  markdown: {
+    rehypePlugins: [[addClasses, {
+      h1: 'H1',
+      h2: 'H2',
+      h3: 'H3',
+      h4: 'H4',
+      code: 'bg-indigoA-3 px-1.5 pt-0.5 rd c-indigoA-11 ',
+      a: 'underline-blueA-11 ',
+    }]]
+  }
 });
